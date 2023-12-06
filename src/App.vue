f564da4d7606c9c321b499eb46f6e267582ff606
skmirajbn <skmirajbn@gmail.com>
2023-12-06 09:46:57 +0600

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <login></login>
+  <div class="toasts">
+    <theToast></theToast>
+    <theToast></theToast>
+    <theToast></theToast>
+  </div>
 </template>
 
 <script>
 import Login from "./components/Login.vue";
+import TheToast from "./components/TheToast.vue";
 export default {
   components: {
     Login,
+    TheToast,
   },
 };
 </script>
--- src/components/TheToast.vue
+++ src/components/TheToast.vue
@@ -1,9 +1,62 @@
 <template>
-  <div></div>
+  <div class="toast">
+    <div class="toast__header">
+      <span>{{ toastType }}</span>
+      <span>x</span>
+    </div>
+
+    <div class="toast__body">{{ message }}</div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    toastType: {
+      type: String,
+      default: "success",
+    },
+    message: {
+      type: String,
+      default: "Toast Message",
+    },
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.toast {
+  width: 299px;
+  box-shadow: 0 0 6px 2px rgba(0, 0, 0, 0.5);
+  min-height: 22px;
+  margin: 11px;
+  background-color: #fff;
+  border-radius: 2px;
+  color: green;
+}
+
+.toast__Error {
+  color: red;
+}
+
+.toast__header {
+  border-radius: 3px 3px 0 0;
+  background-color: rgb(215, 255, 215);
+  font-weight: bold;
+  padding: 5px 11px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.toast__Error .toast__header {
+  background-color: rgb(252, 198, 198);
+}
+
+.toast__body {
+  padding: 11px;
+}
+
+.toast__close {
+  cursor: pointer;
+}
+</style>
